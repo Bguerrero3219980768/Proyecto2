45e23a857e0a48c4f0479f28903156fb1caa6e22
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2024-10-17 19:14:04 -0500

    conexion bd

--- Proyecto2/settings.py
+++ Proyecto2/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "Proyecto2024.apps.Proyecto2024Config"
+    "Proyecto2024.apps.Proyecto2024Config",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
