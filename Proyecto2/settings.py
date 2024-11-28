d915da68328b9cbb28b32ebb95f45ba78510737e
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2024-11-27 20:20:59 -0500

    .

--- Proyecto2/settings.py
+++ Proyecto2/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w=j@ey=ewi8hvnr+ulr1)gng_i#)=weu6xo(zn)3qrc(bi+g8w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
