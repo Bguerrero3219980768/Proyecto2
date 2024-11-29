724b4b3e502fd0e447314e836c83a1ebf06d268a
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2024-11-28 19:27:44 -0500

    arreglo

--- Proyecto2/settings.py
+++ Proyecto2/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w=j@ey=ewi8hvnr+ulr1)gng_i#)=weu6xo(zn)3qrc(bi+g8w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [BASE_DIR / "Proyecto2024" / "static"]
 
